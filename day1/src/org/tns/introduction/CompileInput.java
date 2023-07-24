0f9482da5f77c9f9fd5ba094c9475be09d98df9a
Your Name <you@example.com>
2023-07-24 15:11:58 -0700

    commit day 1

---
+++ day1/src/org/tns/introduction/CompileInput.java
@@ -0,0 +1,14 @@
+//Program to demonstrate on compile time input
+package org.tns.introduction;
+
+
+public class CompileInput {
+
+	public static void main(String[] args) {
+		
+		short num = 34;// var initialization
+		System.out.println("The num is:"+num);
+
+	}
+
+}
